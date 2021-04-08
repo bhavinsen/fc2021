5940da4cf057ae32877e4181633e1411d65a4808
bhavinsen <senbhavin@gmail.com>
2021-04-08 10:03:41 +0530

    heroku 10

--- core/settings.py
+++ core/settings.py
@@ -117,7 +117,7 @@ SITE_ID = 1
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(CORE_DIR, 'core/staticfiles')
+STATIC_ROOT = os.path.join(CORE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.  
