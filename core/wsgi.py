87ca786407208a6a60b5eb97fefcf94c00813e24
bhavinsen <senbhavin@gmail.com>
2021-04-08 09:53:42 +0530

    heroku 8

--- core/wsgi.py
+++ core/wsgi.py
@@ -11,5 +11,5 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_wsgi_application()
 
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
+# from whitenoise.django import DjangoWhiteNoise
+# application = DjangoWhiteNoise(application)
