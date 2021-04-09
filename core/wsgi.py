81e8dd1a71889a5217736e95c44dd16d8d1d1554
bhavinsen <senbhavin@gmail.com>
2021-04-09 10:05:11 +0530

    static files

--- core/wsgi.py
+++ core/wsgi.py
@@ -4,16 +4,12 @@ Copyright (c) 2019 - present AppSeed.us
 """
 
 import os
-from dj_static import Cling
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'new_email.settings')
 
-
-
-
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
 
 # from whitenoise.django import DjangoWhiteNoise
 # application = DjangoWhiteNoise(application)
